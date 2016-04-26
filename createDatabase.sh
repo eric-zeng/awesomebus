c0b55c3c7ff3690d75135e4f9b4a0d961a2d0a28
lucy <asfsdf@email.com>
2016-04-26 14:51:19 -0700

    more streamlined; also make the join

--- createDatabase.sh
+++ createDatabase.sh
@@ -1,3 +1,8 @@
+dropdb transitdata
+rm data/allData.csv
+touch data/allData.csv
+echo "dropped database transitdata just so we are starting fresh"
 createdb transitdata
 echo "CREATED DATABASE transitdata"
-# psql -f createRawGTFSDB.sql transitdata
+psql -f createRawGTFSDB.sql transitdata
+echo "populated transitdata and joined files routes, trips, and shapes to data/allData.csv"
