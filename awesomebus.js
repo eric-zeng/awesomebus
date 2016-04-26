b035845972a3d96426bc0bdaa7bee277b7e0c595
Eric Zeng <ericzeng@cs.washington.edu>
2016-04-26 14:49:37 -0700

    Add Node.js

--- awesomebus.js
+++ awesomebus.js
@@ -1,6 +1,11 @@
 'use strict';
+var postgres = require('ps');
+var d3 = require('d3');
 
 var svg = d3.select("body").append("svg")
     .attr("width", 1024)
     .attr("height", 768);
 
+console.log(d3);
+console.log(postgres);
+console.log('something');
