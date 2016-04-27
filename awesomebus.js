510e02be3d39cdbeb5b3dd01e55411f94e706162
lucy <asfsdf@email.com>
2016-04-26 18:46:37 -0700

    can now talk to the flask server and get back sql requests (minimal functionality currently)

--- awesomebus.js
+++ awesomebus.js
@@ -1,11 +1,22 @@
 'use strict';
-var postgres = require('ps');
 var d3 = require('d3');
 
 var svg = d3.select("body").append("svg")
     .attr("width", 1024)
     .attr("height", 768);
+ 
+var xhr = require('xhr');
 
-console.log(d3);
-console.log(postgres);
-console.log('something');
+var query = 'test'
+
+xhr.get({
+
+    url: 'http://localhost:5000/' + query
+
+}, function (error, response, body) {
+    //console.log(JSON.parse(body));
+    console.log("hello!");
+    console.log(body);
+
+});
+ 
