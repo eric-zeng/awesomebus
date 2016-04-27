b54201e05207a65d3a419598c3a2926fe52fa482
lucy <asfsdf@email.com>
2016-04-26 22:42:40 -0700

    uses POST now and takes a SQL query specified through JSON (through there isn't much flexibility yet in how to structure the SQL query ... will do that tomorrow)

--- awesomebus.js
+++ awesomebus.js
@@ -7,16 +7,36 @@ var svg = d3.select("body").append("svg")
  
 var xhr = require('xhr');
 
-var query = 'test'
-
+var query = 'test';
+var sqlVars = 'select * from shapes limit 10';
+/*
 xhr.get({
 
-    url: 'http://localhost:5000/' + query
+    url: 'http://localhost:5000/' + query  + "/" + sqlVars
 
 }, function (error, response, body) {
     //console.log(JSON.parse(body));
     console.log("hello!");
     console.log(body);
 
+});*/
+
+
+console.log("done with the get");
+xhr.post( {
+    url: 'http://localhost:5000/query',
+    /* CHANGE THIS PART!!!
+    TODO: functionize this??*/
+    json: 
+      [{
+        "select" : "*",
+        "from"  : "routes", 
+        "where" : "route_id='100001'",
+        "limit" : "10"
+      }]
+  }, 
+  function(error, response, body) {
+    /*console.log(body)*/
+    console.log("????!");
 });
  
