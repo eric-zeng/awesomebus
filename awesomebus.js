ff93d9020bd379c3fdc472cd55420a95e8e42b93
Eric Zeng <ericzeng@cs.washington.edu>
2016-04-27 17:54:55 -0700

    Attempt to do simple query - coordinates by route number

--- awesomebus.js
+++ awesomebus.js
@@ -1,25 +1,26 @@
 'use strict';
 var d3 = require('d3');
+var xhr = require('xhr');
 
 var svg = d3.select("body").append("svg")
     .attr("width", 1024)
     .attr("height", 768);
- 
-var xhr = require('xhr');
 
-xhr.post( {
-    url: 'http://localhost:5000/query',
-    /* CHANGE THIS PART!!!
-    TODO: functionize this??*/
-    json: 
-      [{
-        "select" : "routes.route_id, shapes.shape_id",
-        "from"  : "routes, trips, shapes", 
-        "where" : "(routes.route_id='100001' and trips.route_id=routes.route_id) or shapes.shape_id='123123'",
-        "limit" : "10"
-      }]
-  }, 
-  function(error, response, body) {
-    console.log(body)
+let queryUrl = 'http://localhost:5000/query';
+
+function getRouteShapes(routeNum, callback) {
+  xhr.post({
+    url: queryUrl,
+    json: [{
+      'select': 'shapes.shape_pt_lat, shapes.shape_pt_lon',
+      'from': 'routes, trips, shapes',
+      'where': 'routes.route_short_name=' + routeNum + ' AND trips.route_id=routes.route_id AND shapes.shape_id=trips.shape_id'
+    }]
+  }, callback);
+}
+
+
+getRouteShapes(48, function(err, response, body) {
+  console.log(err);
+  console.log(response);
 });
- 
