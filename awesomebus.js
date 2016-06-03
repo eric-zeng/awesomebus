f13357248a5e8a7795a116c96900bc3cdad92294
Eric Zeng <ericzeng@cs.washington.edu>
2016-06-03 01:01:26 -0700

    Tweak the sidebar styles some more

--- awesomebus.js
+++ awesomebus.js
@@ -3,7 +3,7 @@
 /*******     MAP RENDERING        ********************************************/
 /*****************************************************************************/
 // Setup SVG where map will be rendered
-let width = window.innerWidth - 250;
+let width = window.innerWidth - 280;
 let height = window.innerHeight;
 
 // Intialize map div
